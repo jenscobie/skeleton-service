9a650aefc1644c74f26def03289144e692a5548b
jenscobie <jenscobie@gmail.com>
2015-05-04 13:43:29 -0700

    Example of contract test with spock

---
+++ src/test/groovy/HelloWorld.groovy
@@ -0,0 +1,16 @@
+import groovyx.net.http.RESTClient
+import spock.lang.*
+
+class HelloWorldContract extends spock.lang.Specification {
+
+    def "GET returns 200"() {
+        given:
+        def client = new RESTClient('http://localhost:8080')
+
+        when:
+        def response = client.get('hello-world')
+
+        then:
+        response.status == 200
+    }
+}
